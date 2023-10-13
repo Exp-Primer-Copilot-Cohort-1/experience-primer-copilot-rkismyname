9923ff282f0a0d03eea3bed3c5066445c2f129c6
rkismyname <arkadiorjndra@gmail.com>
2023-10-13 14:17:51 +0700

    Copilot third commit

--- comment.js
+++ comment.js
@@ -1,111 +1,53 @@
 // Create web server
+// Create database connection
+// Create routes
+// Create API
+// Create test
+// Create UI
 
-// Load modules
+// 1. Create web server
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var path = require('path');
+var cors = require('cors');
+
+// 2. Create database connection
 var mongoose = require('mongoose');
-
-// Connect to Mongoose
-mongoose.connect('mongodb://localhost/comment');
-var db = mongoose.connection;
-
-// Check connection
-db.once('open', function(){
-	console.log('Connected to MongoDB');
-});
-
-// Check for DB errors
-db.on('error', function(err){
-	console.log(err);
-});
-
-// Bring in models
+mongoose.connect('mongodb://localhost/comment', { useNewUrlParser: true });
 var Comment = require('./models/comment');
 
-// Load view engine
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-// Body parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
+// 3. Create routes
+app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// Set public folder
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Home route
-app.get('/', function(req, res){
-	Comment.find({}, function(err, comments){
-		if(err){
-			console.log(err);
-		} else {
-			res.render('index', {
-				title: 'Comments',
-				comments: comments
-			});
-		}
-	});
-});
-
-// Add comment route
-app.get('/comment/add', function(req, res){
-	res.render('add_comment', {
-		title: 'Add Comment'
-	});
-});
-
-// Add submit POST route
-app.post('/comments/add', function(req, res){
-	var comment = new Comment();
-	comment.name = req.body.name;
-	comment.comment = req.body.comment;
-
-	comment.save(function(err){
-		if(err){
-			console.log(err);
-			return;
-		} else {
-			res.redirect('/');
-		}
-	});
-});
-
-// Edit comment route
-app.get('/comment/edit/:id', function(req, res){
-	Comment.findById(req.params.id, function(err, comment){
-		res.render('edit_comment', {
-			title: 'Edit Comment',
-			comment: comment
-		});
-	});
-});
-
-// Edit submit POST route
-app.post('/comments/edit/:id', function(req, res){
-	var comment = {};
-	comment.name = req.body.name;
-	comment.comment = req.body.comment;
-
-	var query = {_id:req.params.id}
-
-	Comment.update(query, comment, function(err){
-		if(err){
-			console.log(err);
-			return;
-		} else {
-			res.redirect('/');
-		}
-	});
-});
-
-// Delete comment route
-app.delete('/comment/:id', function(req, res){
-	var query = {_id};
-    Comment.remove(query, function(err){
-        if(err){
-            console.log(err);
+// 4. Create API
+// GET
+app.get('/comments', function(req, res) {
+    Comment.find(function(err, comments) {
+        if (err) {
+            res.send(err);
         }
-        res.send('Success');
-    });});
+        res.json(comments);
+    });
+});
+
+// POST
+app.post('/comments', function(req, res) {
+    var comment = new Comment();
+    comment.author = req.body.author;
+    comment.text = req.body.text;
+
+    comment.save(function(err) {
+        if (err) {
+            res.send(err);
+        }
+        res.json({ message: 'Comment successfully added!' });
+    });
+});
+
+// 5. Create test
+// 6. Create UI
+app.listen(3000, function() {
+    console.log('Simple comment system running on port 3000!');
+});
